25cfa061a5422adbfec5b896d506796c12b9f820
William Hobson <will.hobson85@gmail.com>
2022-05-13 22:34:17 -0400

    Complete library tests.

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,37 @@
+function createLibrary(libName, bookShelf) {
+  var publicLibrary = {
+    name: libName,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    }
+  }
+  return publicLibrary;
+}
 
+function addBook(library, book) {
+  if (book.genre === 'fantasy') {
+    library.shelves.fantasy.push(book)
+  } else if (book.genre === 'fiction') {
+    library.shelves.fiction.push(book)
+  } else {
+    library.shelves.nonFiction.push(book)
+  }
+}
+
+function checkoutBook(library, book, genre) {
+  for (var i = 0; i < library.shelves[genre].length; i++) {
+    if(library.shelves[genre][i].title === book) {
+      library.shelves[genre].splice(i, 1);
+      return `You have now checked out ${book} from the ${library.name}`;
+    }
+  }
+  return `Sorry, there are currently no copies of ${book} available at the ${library.name}`
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
